efb4a02eec6f9f063e041c5133ab558988f22b7e
nikolay <nikolay@supermicro.pu.ru>
2018-01-29 13:01:59 +0300

    gwatch.conf fixed

--- src/prod.php
+++ src/prod.php
@@ -16,7 +16,7 @@ $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'host'     => '127.0.0.1',
     'port'     => '3306',
-    'dbname'   => 'Module_S',
+    'dbname'   => 'GWATCH',
     'user'     => 'gwatch',
     'password' => 'gwatch',
 );
